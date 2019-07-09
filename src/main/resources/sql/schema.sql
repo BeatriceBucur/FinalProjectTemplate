d3d603c9f962b3b2a46f39641e38a275aead4b6b
Beatrice Bucur <beatrice.bucur@gmail.com>
2019-07-10 01:17:37 +0300

    schema.sql contains sql code to regenerate the BarberShop schema

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,25 @@
+create schema if not exists barbershop collate utf8mb4_0900_ai_ci;
+
+use barbershop;
+
+create table if not exists reservation
+(
+    reservation_id int null,
+    client_name char(30) not null,
+    client_email char(50) not null,
+    client_mobile int(10) not null,
+    location_id int(10) not null,
+    workitem_id int(10) not null
+);
+
+create table if not exists workitem
+(
+    workitem_id int not null,
+    workitem_name char(15) not null,
+    workitem_fee int(3) not null,
+    workitem_duration int(2) not null
+)
+
+INSERT INTO barbershop.workitem (workitem_id, workitem_name, workitem_fee, workitem_duration) VALUES (1, 'haircut', 30, 20);
+INSERT INTO barbershop.workitem (workitem_id, workitem_name, workitem_fee, workitem_duration) VALUES (2, 'shave', 50, 20);
+INSERT INTO barbershop.workitem (workitem_id, workitem_name, workitem_fee, workitem_duration) VALUES (3, 'haircutAndShave', 80, 40);
