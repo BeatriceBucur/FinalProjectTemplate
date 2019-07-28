0b92dc54b7e115ab7ee6a0856d276045a75a7b24
Catalina <cristea.catalina89@gmail.com>
2019-07-28 20:18:53 +0300

    i put a method findByName

--- src/main/java/sda/repository/LocationRepository.java
+++ src/main/java/sda/repository/LocationRepository.java
@@ -1,5 +1,6 @@
 package sda.repository;
 
+import org.omg.PortableInterceptor.LOCATION_FORWARD;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sda.entity.Location;
 
@@ -7,6 +8,6 @@ import java.util.List;
 
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 
-
+    Location findByName(String name);
 
 }
